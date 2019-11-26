82837bf27125a39d8f6f774fae6370810bfc6f44
leejunghyun <cxz9080@gmail.com>
2019-11-26 09:31:08 +0900

    프로토타입 개발완료.

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,9 @@ var {Tobacco} = require('../models');
 
 router.get('/', async (req, res) => {
     var tobacco_list = await Tobacco.findAll();
-    let session = req.session;
+    var session = req.session;
+
+    var brand_list = tobacco_list;
 
     //구름 객체들을 별점을 기준으로 sorting하여 배열에 담아 context로 보냄.
     var best_tobacco = []; 
@@ -19,7 +21,7 @@ router.get('/', async (req, res) => {
     best_tobacco = best_tobacco.slice(0, 4);
 
 
-    res.render('home.ejs', {'tobacco_list':best_tobacco, "session":session});
+    res.render('home.ejs', {'tobacco_list':best_tobacco, "session":session, "brand_list":brand_list});
 });
 
 module.exports = router;
